61c5535c6f7964fed0aa512769a5994a01026f32
Kristina Chodorow <k@ubuntu.(none)>
2010-08-18 14:27:49 -0400

    make linger work on old compilers

--- mongo.c
+++ mongo.c
@@ -2210,6 +2210,9 @@ static int php_mongo_connect_nonb(mongo_server *server, int timeout, zval *errms
   int family;
   struct timeval tval;
   int connected = FAILURE, status = FAILURE;
+#ifdef SO_LINGER
+  struct linger ling;
+#endif
 
 #ifdef WIN32
   WORD version;
@@ -2270,7 +2273,6 @@ static int php_mongo_connect_nonb(mongo_server *server, int timeout, zval *errms
   }
 
 #ifdef SO_LINGER
-  struct linger ling;
   ling.l_onoff = 1;
   ling.l_linger = 0;
   setsockopt(server->socket, SOL_SOCKET, SO_LINGER, (char *) &ling, sizeof(ling));
