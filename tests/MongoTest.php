8684b8a2a14587c614d8877a1cdfacbd6ac207b6
Kristina <kristina@10gen.com>
2011-02-07 16:10:52 -0500

    BUMP 1.1.4

--- tests/MongoTest.php
+++ tests/MongoTest.php
@@ -8,7 +8,7 @@ require_once 'PHPUnit/Framework.php';
 class MongoTest extends PHPUnit_Framework_TestCase
 {
     public function testVersion() {
-        $this->assertEquals("1.1.3", Mongo::VERSION);
+        $this->assertEquals("1.1.4", Mongo::VERSION);
     }
 
     /**
