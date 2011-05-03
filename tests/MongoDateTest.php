7e9dd703d62fc6c4b3248bcb99a8c9e0fe2466eb
Kristina <kristina@10gen.com>
2011-05-03 10:51:34 -0400

    set timezone in test

--- tests/MongoDateTest.php
+++ tests/MongoDateTest.php
@@ -15,6 +15,7 @@ class MongoDateTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($d1->usec, $d2->usec);
         $this->assertEquals($t, $d1->sec);
 
+        date_default_timezone_set('America/New_York');
         $d3 = new MongoDate(strtotime('2009-05-01 00:00:00'));
         $this->assertEquals(1241150400, $d3->sec);
         $this->assertEquals(0, $d3->usec);
