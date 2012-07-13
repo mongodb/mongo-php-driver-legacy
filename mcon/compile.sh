cfd809e5de6cb98d28b2273e7e5176fbf902f4ff
Derick Rethans <github@derickrethans.nl>
2012-07-13 12:29:09 +0100

    Put this back in the compile script.

--- mcon/compile.sh
+++ mcon/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #gcc -ggdb3 -O0 -o test2 bson_helpers.c connections.c manager.c mini_bson.c parse.c simplecon-test.c str.c utils.c io.c
-#gcc -Wall -ggdb3 -O0 -o test3 bson_helpers.c collection.c connections.c manager.c mini_bson.c parse.c read_preference.c replicacon-test.c str.c utils.c io.c
+gcc -Wall -ggdb3 -O0 -o test3 bson_helpers.c collection.c connections.c manager.c mini_bson.c parse.c read_preference.c replicacon-test.c str.c utils.c io.c
 gcc -Wall -ggdb3 -O0 -o rp-test1 rp-test-simple1.c bson_helpers.c collection.c connections.c manager.c mini_bson.c parse.c read_preference.c str.c utils.c io.c
