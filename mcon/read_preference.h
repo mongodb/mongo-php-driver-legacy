28ad52027818f762acebe4813672f6b96fa70f47
Derick Rethans <github@derickrethans.nl>
2012-07-26 14:37:42 +0100

    Make sure MONGO_RP_PRIMARY is value 0, saves us from having to explicitly set it all over the place.

--- mcon/read_preference.h
+++ mcon/read_preference.h
@@ -4,11 +4,11 @@
 #include "types.h"
 #include "collection.h"
 
-#define MONGO_RP_PRIMARY             0x01
-#define MONGO_RP_PRIMARY_PREFERRED   0x02
-#define MONGO_RP_SECONDARY           0x03
-#define MONGO_RP_SECONDARY_PREFERRED 0x04
-#define MONGO_RP_NEAREST             0x05
+#define MONGO_RP_PRIMARY             0x00
+#define MONGO_RP_PRIMARY_PREFERRED   0x01
+#define MONGO_RP_SECONDARY           0x02
+#define MONGO_RP_SECONDARY_PREFERRED 0x03
+#define MONGO_RP_NEAREST             0x04
 
 /* FIXME: Needs to be a setting through the connection string/options */
 #define MONGO_RP_CUTOFF  15
