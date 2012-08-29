5c55098f89054c82b9a18ba4d9ad319c938aec97
Derick Rethans <github@derickrethans.nl>
2012-08-29 08:58:11 +0100

    Updates to test file.

--- mcon/tests/authcon-test.c
+++ mcon/tests/authcon-test.c
@@ -18,11 +18,20 @@ int main(void)
 	servers = mongo_parse_init();
 	mongo_parse_server_spec(manager, servers, "mongodb://127.0.0.1:27017", (char**) &error_message);
 	mongo_servers_dump(manager, servers);
+
 	con = mongo_get_read_write_connection(manager, servers, MONGO_CON_FLAG_READ, (char**) &error_message);
 	nonce = mongo_connection_getnonce(manager, con, (char**) &error_message);
 	printf("The nonce is: %s\n", nonce);
+	if (!mongo_connection_authenticate(manager, con, "phpunit", "admin", "wrong!", nonce, (char**) &error_message)) {
+		printf("ERROR: %s\n", error_message);
+		free(error_message);
+		mongo_manager_connection_deregister(manager, con);
+	}
 
-	if (!mongo_connection_authenticate(manager, con, "phpunit", "admin", "admin", "c610366174df1bec", (char**) &error_message)) {
+	con = mongo_get_read_write_connection(manager, servers, MONGO_CON_FLAG_READ, (char**) &error_message);
+	nonce = mongo_connection_getnonce(manager, con, (char**) &error_message);
+	printf("The nonce is: %s\n", nonce);
+	if (!mongo_connection_authenticate(manager, con, "phpunit", "admin", "admin", nonce, (char**) &error_message)) {
 		printf("ERROR: %s\n", error_message);
 		free(error_message);
 	}
