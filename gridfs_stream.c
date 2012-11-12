893a5d08660c0cef64195434fbc5434622bdfce2
Derick Rethans <github@derickrethans.nl>
2012-11-12 13:18:02 +0000

    Implemented PHP-565: Add license files to mcon/
    Also fixed the copyright year in the other files, and added a LICENSE.md file.

--- gridfs_stream.c
+++ gridfs_stream.c
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2009-2011 10gen, Inc.
+ *  Copyright 2009-2012 10gen, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -12,8 +12,9 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
+ * Author: César D. Rodas <crodas@php.net>
  */
-/* Author: César D. Rodas <crodas@php.net> */
 #include <php.h>
 #ifdef WIN32
 #ifndef int64_t
