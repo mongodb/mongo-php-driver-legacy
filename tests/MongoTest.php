06f1342c4085fc5ef0b61007b395205c81c9da6f
Derick Rethans <github@derickrethans.nl>
2012-11-20 14:28:02 +0000

    BUMP 1.3.0RC3.

--- tests/MongoTest.php
+++ tests/MongoTest.php
@@ -6,7 +6,7 @@
 class MongoTest extends PHPUnit_Framework_TestCase
 {
     public function testVersion() {
-        $this->assertEquals("1.3.0RC3-dev", Mongo::VERSION);
+        $this->assertEquals("1.3.0RC3", Mongo::VERSION);
     }
 
     /**
