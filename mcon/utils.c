e9c81022755370b08471ae78357e61769a120940
Derick Rethans <github@derickrethans.nl>
2012-12-03 18:23:15 +0000

    Add licence clauses and re-format code according to our CS guidelines.

--- mcon/utils.c
+++ mcon/utils.c
@@ -489,19 +489,50 @@ void MD5_Final(unsigned char *result, MD5_CTX *ctx)
 	memset(ctx, 0, sizeof(*ctx));
 }
 
-/* Compat stuff for Windows */
+/*
+ * Copyright (c) 1988, 1993
+ *      The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* Compat function for systems that do not have strndup().
+ * This is borrowed from FreeBSD's strndup.c, with minor CS changes */
 char *mcon_strndup(char *str, size_t n)
 {
-	/* Borrowed from FreeBSD strndup.c */
 	size_t len;
 	char *copy;
 
-	for (len = 0; len < n && str[len]; len++)
+	for (len = 0; len < n && str[len]; len++) {
 		continue;
+	}
 
-	if ((copy = malloc(len + 1)) == NULL)
-		return (NULL);
+	if ((copy = malloc(len + 1)) == NULL) {
+		return NULL;
+	}
 	memcpy(copy, str, len);
 	copy[len] = '\0';
-	return (copy);
+	return copy;
 }
