775daae31cea1fd0fd42686ffd06081d08fde456
Derick Rethans <github@derickrethans.nl>
2012-12-18 10:57:44 +0000

    BUMP 1.3.2

--- php_mongo.h
+++ php_mongo.h
@@ -16,7 +16,7 @@
 #ifndef PHP_MONGO_H
 #define PHP_MONGO_H 1
 
-#define PHP_MONGO_VERSION "1.3.2RC2-dev"
+#define PHP_MONGO_VERSION "1.3.2"
 #define PHP_MONGO_EXTNAME "mongo"
 
 #include "mcon/types.h"
