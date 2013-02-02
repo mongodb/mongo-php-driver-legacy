448349eaaccf0f96c34289e636db2d47672e122e
Hannes Magnusson <bjori@10gen.com>
2013-02-02 15:18:52 -0800

    Improve bootstrapping and add some options

--- tests/utils/make-servers.php
+++ tests/utils/make-servers.php
@@ -1,6 +1,13 @@
 <?php
+/*
+ * The only bootup specific type of servers set
+ *      MONGO_SERVER_[SERVER_TYPE]=yes
+ * in your environment before running this script.
+ *
+ * To bootup all exception a specific type of server set
+ *      SKIP_MONGO_SERVER_[SERVER_TYPE]=yes
+ */
 require_once "tests/utils/server.inc";
-mkdir("/data/db", 0777, true);
 
 function t() {
     static $last;
@@ -12,80 +19,109 @@ function t() {
     }
     $last = microtime(true);
 }
-$retry = true;
-do {
-    try {
-        $server = new MongoShellServer;
-
-
-        echo "Making Standalone.... ";
-        t();
-        $standalone = microtime(true);
+function makeServer($SERVERS, $server, $bit) {
+    echo "Making " . $SERVERS[$bit] . ".. ";
+    t();
+    switch($bit) {
+    case STANDALONE:
         $server->makeStandalone(30000);
-        sprintf("DONE (%.2f secs)\n", t());
-        $str = $server->getStandaloneConfig();
-        // Travis fail fast
-        if (strlen($str) == 5) {
-            var_dump($str);
-            exit(2);
-        }
-
-
-        exit(0);
-        echo "Making Authenticated Standalone.... ";
-        t();
-        $standalone = microtime(true);
+        $dsn = $server->getStandaloneConfig();
+        break;
+    case STANDALONE_AUTH:
         $server->makeStandalone(30100, true);
-        sprintf("DONE (%.2f secs)\n", t());
-        var_dump($server->getStandaloneConfig(true));
-
-
-        echo "Making Bridge.... ";
-        echo "doen't work in travis, mongobridge is not installed by default\n";
-        /*
+        $dsn = $server->getStandaloneConfig(true);
+        break;
+    case STANDALONE_BRIDGE:
         $sc = $server->getStandaloneConfig();
         list($shost, $sport) = explode(":", trim($sc));
         $server->makeBridge($sport, 1000);
-        printf("DONE (%.2f secs)\n", t());
-        var_dump($server->getBridgeConfig());
-         */
-
-
-        echo "Making shard.... ";
+        $dsn = $server->getBridgeConfig();
+        break;
+    case MONGOS:
         $server->makeShard(2);
-        printf("DONE (%.2f secs)\n", t());
-        var_dump($server->getShardConfig());
-
-        echo "Making ReplicaSet.... ";
+        $cfg = $server->getShardConfig();
+        $dsn = join(",", $cfg);
+        break;
+    case REPLICASET:
         $server->makeReplicaset(4, 30200);
-        printf("DONE (%.2f secs)\n", t());
-        var_dump($server->getReplicaSetConfig());
-
-        echo "Making Authenticated ReplicaSet.... ";
-        $server->makeReplicaset(4, 30300, dirname(__FILE__) . "/keyFile");
-        printf("DONE (%.2f secs)\n", t());
-        var_dump($server->getReplicaSetConfig(true));
-
-        $server->close();
-    } catch(Exception $e) {
-        if ($retry) {
-            $retry = false;
-            $pid = pcntl_fork();
-            if ($pid > 0) {
-                sleep(1);
-                continue;
-            }
-            if (!$pid) {
-                posix_setsid();
-                require_once dirname(__FILE__) . "/daemon.php";
-                exit(0);
-            }
-        }
-
-        echo $e->getMessage(), "\n";
-        exit(1);
+        $cfg = $server->getReplicaSetConfig();
+        $dsn = $cfg["dsn"];
+        break;
+    case REPLICASET_AUTH:
+        $retval = $server->makeReplicaset(4, 30300, dirname(__FILE__) . "/keyFile");
+        var_dump($retval);
+        $cfg = $server->getReplicaSetConfig(true);
+        var_dump($cfg);
+        $dsn = $cfg["dsn"];
+        break;
+    default:
+        var_dump("No idea what to do about $bit");
+        exit(32);
     }
-    echo "We have liftoff \n";
-    break;
-} while(true);
+    printf("DONE (%.2f secs): %s\n", t(), $dsn);
+}
+
+$SERVERS = array(
+    "STANDALONE"        => 0x01,
+    "STANDALONE_AUTH"   => 0x02,
+    "STANDALONE_BRIDGE" => 0x04,
+    "MONGOS"            => 0x08,
+    "REPLICASET"        => 0x10,
+    "REPLICASET_AUTH"   => 0x20,
+);
+
+$ALL_SERVERS = 0;
+$BOOTSTRAP = 0;
+foreach($SERVERS as $server => $bit) {
+    define($server, $bit);
+    $ALL_SERVERS |= $bit;
+}
+
+foreach($SERVERS as $server => $bit) {
+    if (getenv("MONGO_SERVER_$server")) {
+        $BOOTSTRAP |= $bit;
+    }
+}
+if (!$BOOTSTRAP) {
+    $BOOTSTRAP = $ALL_SERVERS;
+    foreach($SERVERS as $server => $bit) {
+        if (getenv("SKIP_MONGO_SERVER_$server")) {
+            $BOOTSTRAP &= ~$bit;
+        }
+    }
+}
+function makeDaemon() {
+    $pid = pcntl_fork();
+    if ($pid > 0) {
+        sleep(1);
+        return;
+    }
+    if (!$pid) {
+        posix_setsid();
+        require_once dirname(__FILE__) . "/daemon.php";
+        exit(0);
+    }
+}
+
+try {
+    $server = new MongoShellServer;
+} catch(Exception $e) {
+    makeDaemon();
+    try {
+        $server = new MongoShellServer;
+    } catch(Exception $e)  {
+        echo $e->getMessage();
+        exit(2);
+    }
+}
+
+foreach($SERVERS as $k => $bit) {
+    if ($BOOTSTRAP & $bit) {
+        makeServer(array_flip($SERVERS), $server, $bit);
+    }
+}
+
+$server->close();
+
+echo "We have liftoff \n";
 
