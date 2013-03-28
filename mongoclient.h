d5bf0ddf3ed32a2ff4063a16a8d51e92241115bb
Hannes Magnusson <bjori@10gen.com>
2013-03-27 20:54:36 -0700

    Bump copyright

--- mongoclient.h
+++ mongoclient.h
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2009-2012210gen, Inc.
+ *  Copyright 2009-2013210gen, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
