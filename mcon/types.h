51e3629dbf5c20cdc8c1776543bc8763e0df6622
Derick Rethans <github@derickrethans.nl>
2013-04-26 11:50:57 +0100

    Fixed some warnings and make sure PG and EG can be accessed.

--- mcon/types.h
+++ mcon/types.h
@@ -26,9 +26,15 @@ typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
-#define strcasecmp(a,b) stricmp((a), (b))
-#define snprintf _snprintf
-#define va_copy(d,s) ((void)((d) = (s)))
+#ifndef strcasecmp
+# define strcasecmp(a,b) stricmp((a), (b))
+#endif
+#ifndef snprintf
+# define snprintf _snprintf
+#endif
+#ifndef va_copy
+# define va_copy(d,s) ((void)((d) = (s)))
+#endif
 #else
 # include <stdint.h>
 # include <netinet/in.h>
