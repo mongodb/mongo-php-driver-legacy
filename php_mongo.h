b4b27d2999491ffa1ec021e4c52ac7175059c4f3
Derick Rethans <github@derickrethans.nl>
2013-05-08 10:39:03 +0100

    Fixed PHP-800: Do not overload error codes.
    Making tests for this is tricky, as you need a > 64MB document... not sure if
    it's even worth testing.

--- php_mongo.h
+++ php_mongo.h
@@ -679,7 +679,10 @@ extern zend_module_entry mongo_module_entry;
  * 18: Trying to get more, but cannot find server
  * 19: max number of retries exhausted, couldn't send query
  * 20: something exceptional has happened, and the cursor is now dead
+ * 21: invalid string length for key "%s"
+ * 22: invalid binary length for key "%s"
  * 23: Can not natively represent the long %llu on this platform
+ * 24: invalid code length for key "%s"
  * various: database error
  *
  * MongoGridFSException:
