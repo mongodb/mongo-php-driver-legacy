mongodb/mongo-php-driver-legacy

commit 926d08d6965e88a07e2c9c0cd029484142a79c4e

Remove MONGO_METHOD() for DBRef creation

collection.c
@@ -1422,13 +1422,18 @@ PHP_METHOD(MongoCollection, createDBRef)
 {
 	zval *obj;
 	mongo_collection *c;
+	mongo_db *db;
+	zval *retval;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z", &obj) == FAILURE) {
 		return;
 	}
 
 	PHP_MONGO_GET_COLLECTION(getThis());
-	MONGO_METHOD2(MongoDB, createDBRef, return_value, c->parent, c->name, obj);
+	PHP_MONGO_GET_DB(c->parent);
+
+	retval = php_mongo_dbref_create(obj, Z_STRVAL_P(c->ns), Z_STRVAL_P(db->name));
+	RETURN_ZVAL(retval, 0, 1);
 }
 /* }}} */
 

db.c
@@ -616,23 +616,20 @@ PHP_METHOD(MongoDB, getCollectionNames)
 
 PHP_METHOD(MongoDB, createDBRef)
 {
-	zval *ns, *obj;
-	zval **id;
+	char *ns;
+	int ns_len;
+	zval *obj, *retval = NULL;
+	mongo_db *db;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "zz", &ns, &obj) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz", &ns, &ns_len, &obj) == FAILURE) {
 		return;
 	}
 
-	if (Z_TYPE_P(obj) == IS_ARRAY || Z_TYPE_P(obj) == IS_OBJECT) {
-		if (zend_hash_find(HASH_P(obj), "_id", 4, (void**)&id) == SUCCESS) {
-			MONGO_METHOD2(MongoDBRef, create, return_value, NULL, ns, *id);
-			return;
-		} else if (Z_TYPE_P(obj) == IS_ARRAY) {
-			return;
-		}
-	}
+	PHP_MONGO_GET_DB(getThis());
 
-	MONGO_METHOD2(MongoDBRef, create, return_value, NULL, ns, obj);
+	retval = php_mongo_dbref_create(obj, ns, Z_STRVAL_P(db->name));
+
+	RETURN_ZVAL(retval, 0, 1);
 }
 
 PHP_METHOD(MongoDB, getDBRef)

types/db_ref.c
@@ -19,8 +19,9 @@
 #include "../mongoclient.h"
 #include "../db.h"
 #include "../collection.h"
+#include "db_ref.h"
 
-extern zend_class_entry *mongo_ce_DB, *mongo_ce_Exception;
+extern zend_class_entry *mongo_ce_DB, *mongo_ce_Id, *mongo_ce_Exception;
 
 zend_class_entry *mongo_ce_DBRef = NULL;
 
@@ -30,29 +31,54 @@ zend_class_entry *mongo_ce_DBRef = NULL;
  * array( '$ref' => <collection>, '$id' => <id>[, $db => <dbname>] ) */
 PHP_METHOD(MongoDBRef, create)
 {
-	zval *zns, *zid, *zdb = 0;
+	char *ns, *db = NULL;
+	int ns_len, db_len = 0;
+	zval *zid, *retval;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "zz|z", &zns, &zid, &zdb) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz|s", &ns, &ns_len, &zid, &db, &db_len) == FAILURE) {
 		return;
 	}
 
-	array_init(return_value);
+	retval = php_mongo_dbref_create(zid, ns, db);
+	RETURN_ZVAL(retval, 0, 1);
+}
+
+zval *php_mongo_dbref_create(zval *zid, char *ns, char *db)
+{
+	zval *retval;
+
+	if (Z_TYPE_P(zid) == IS_OBJECT && instanceof_function(Z_OBJCE_P(zid), mongo_ce_Id TSRMLS_CC)) {
+		convert_to_string_ex(&zid);
+	} else if (Z_TYPE_P(zid) == IS_ARRAY || Z_TYPE_P(zid) == IS_OBJECT) {
+		zval **tmpval;
+
+		if (zend_hash_find(HASH_P(zid), "_id", 4, (void**)&tmpval) == SUCCESS) {
+			return php_mongo_dbref_create(*tmpval, ns, db);
+		} else {
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Cannot find _id key in the %s", zend_get_type_by_const(Z_TYPE_P(zid)));
+			return NULL;
+		}
+	} else if (Z_TYPE_P(zid) == IS_RESOURCE) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Don't know what to do with a resource type");
+		return NULL;
+	}
+
+	MAKE_STD_ZVAL(retval);
+	array_init(retval);
 
 	/* add collection name */
-	convert_to_string(zns);
-	add_assoc_zval(return_value, "$ref", zns);
-	zval_add_ref(&zns);
+	add_assoc_string(retval, "$ref", ns, 1);
 
 	/* add id field */
-	add_assoc_zval(return_value, "$id", zid);
+	add_assoc_zval(retval, "$id", zid);
 	zval_add_ref(&zid);
 
 	/* if we got a database name, add that, too */
-	if (zdb) {
-		convert_to_string(zdb);
-		add_assoc_zval(return_value, "$db", zdb);
-		zval_add_ref(&zdb);
+	if (db) {
+		add_assoc_string(retval, "$db", db, 1);
 	}
+
+	return retval;
 }
 /* }}} */
 

types/db_ref.h
@@ -16,6 +16,8 @@
 #ifndef __TYPES_DB_REF_H__
 #define __TYPES_DB_REF_H__
 
+zval *php_mongo_dbref_create(zval *zid, char *ns, char *db);
+
 PHP_METHOD(MongoDBRef, create);
 PHP_METHOD(MongoDBRef, isRef);
 PHP_METHOD(MongoDBRef, get);

commit b019f2e8273b709f3386b53648b5dbb953eec405

Fixed PHP-883: createDBRef() collection/DB method should omit $db
This restores original behavior, which was broken by 926d08d6965e88a07e2c9c0cd029484142a79c4e.

collection.c
@@ -1450,7 +1450,7 @@ PHP_METHOD(MongoCollection, createDBRef)
 	PHP_MONGO_GET_COLLECTION(getThis());
 	PHP_MONGO_GET_DB(c->parent);
 
-	retval = php_mongo_dbref_create(obj, Z_STRVAL_P(c->ns), Z_STRVAL_P(db->name) TSRMLS_CC);
+	retval = php_mongo_dbref_create(obj, Z_STRVAL_P(c->name), NULL TSRMLS_CC);
 	RETURN_ZVAL(retval, 0, 1);
 }
 /* }}} */

db.c
@@ -627,7 +627,7 @@ PHP_METHOD(MongoDB, createDBRef)
 
 	PHP_MONGO_GET_DB(getThis());
 
-	retval = php_mongo_dbref_create(obj, ns, Z_STRVAL_P(db->name) TSRMLS_CC);
+	retval = php_mongo_dbref_create(obj, ns, NULL TSRMLS_CC);
 
 	RETURN_ZVAL(retval, 0, 1);
 }

tests/generic/mongocollection-createdbref-001.phpt
@@ -1,0 +1,35 @@
+--TEST--
+MongoCollection::createDBRef()
+--SKIPIF--
+<?php require_once "tests/utils/standalone.inc"; ?>
+--FILE--
+<?php require_once "tests/utils/server.inc"; ?>
+<?php
+
+$dsn = MongoShellServer::getStandaloneInfo();
+$mongo = new MongoClient($dsn);
+$id = new MongoId('5202e48be84df152458b4567');
+
+var_dump($mongo->database->collection->createDBRef($id));
+var_dump($mongo->database->collection->createDBRef(array('_id' => $id)));
+
+?>
+--EXPECTF--
+array(2) {
+  ["$ref"]=>
+  string(10) "collection"
+  ["$id"]=>
+  object(MongoId)#%d (1) {
+    ["$id"]=>
+    string(24) "5202e48be84df152458b4567"
+  }
+}
+array(2) {
+  ["$ref"]=>
+  string(10) "collection"
+  ["$id"]=>
+  object(MongoId)#%d (1) {
+    ["$id"]=>
+    string(24) "5202e48be84df152458b4567"
+  }
+}

tests/generic/mongodb-createdbref-001.phpt
@@ -1,0 +1,35 @@
+--TEST--
+MongoDB::createDBRef()
+--SKIPIF--
+<?php require_once "tests/utils/standalone.inc"; ?>
+--FILE--
+<?php require_once "tests/utils/server.inc"; ?>
+<?php
+
+$dsn = MongoShellServer::getStandaloneInfo();
+$mongo = new MongoClient($dsn);
+$id = new MongoId('5202e48be84df152458b4567');
+
+var_dump($mongo->database->createDBRef('collection', $id));
+var_dump($mongo->database->createDBRef('collection', array('_id' => $id)));
+
+?>
+--EXPECTF--
+array(2) {
+  ["$ref"]=>
+  string(10) "collection"
+  ["$id"]=>
+  object(MongoId)#%d (1) {
+    ["$id"]=>
+    string(24) "5202e48be84df152458b4567"
+  }
+}
+array(2) {
+  ["$ref"]=>
+  string(10) "collection"
+  ["$id"]=>
+  object(MongoId)#%d (1) {
+    ["$id"]=>
+    string(24) "5202e48be84df152458b4567"
+  }
+}