b7b6a4cd053f72728ba503f776dcf787bfd48314
Jeremy Mikola <jmikola@gmail.com>
2014-07-28 21:10:55 -0400

    PHP-1107: Explicitly set connectTimeoutMS default value (60000)
    Previously, the connectTimeoutMS default was only inferred in mongo_connection_connect() (1 second) and php_mongo_io_stream_connect() (no timeout, but defer to default_socket_timeout INI setting). This explicitly uses the default_socket_timeout default (60000) for connectTimeoutMS.

--- mcon/parse.c
+++ mcon/parse.c
@@ -40,7 +40,7 @@ mongo_servers* mongo_parse_init(void)
 	servers->options.repl_set_name = NULL;
 	servers->options.con_type = MONGO_CON_TYPE_STANDALONE;
 
-	servers->options.connectTimeoutMS = 0;
+	servers->options.connectTimeoutMS = MONGO_CONNECTION_DEFAULT_CONNECT_TIMEOUT;
 	servers->options.socketTimeoutMS = MONGO_CONNECTION_DEFAULT_SOCKET_TIMEOUT;
 	servers->options.secondaryAcceptableLatencyMS = MONGO_RP_DEFAULT_ACCEPTABLE_LATENCY_MS;
 	servers->options.default_w = -1;
