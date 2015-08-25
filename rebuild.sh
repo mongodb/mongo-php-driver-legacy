5964e77e67969224b155694aefa9dc712aadbda2
Jeremy Mikola <jmikola@gmail.com>
2015-08-25 13:21:42 -0400

    Suppress stdout in rebuild.sh script
    This makes config and build warnings more visible

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export CFLAGS="-Wall -Wextra -Wdeclaration-after-statement -Wmissing-field-initializers -Wshadow -Wno-unused-parameter -ggdb3"
-phpize && ./configure --with-mongo-sasl && make clean && make -j 5 all && make install
+phpize && ./configure --with-mongo-sasl > /dev/null && \
+make clean && make -j 5 all > /dev/null && make install
