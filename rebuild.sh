78164e4c192e338ffda5f76f1f13b8de944902c8
Jeremy Mikola <jmikola@gmail.com>
2016-03-15 14:18:08 -0400

    Bump make parallelism and suppress stdout in rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export CFLAGS="-Wall -Wextra -Wdeclaration-after-statement -Wmissing-field-initializers -Wshadow -Wno-unused-parameter -ggdb3"
-phpize && ./configure --with-mongo-sasl > /dev/null && \
-make clean && make -j 5 all > /dev/null && make install
+phpize > /dev/null && \
+./configure --with-mongo-sasl > /dev/null && \
+make clean && make all > /dev/null && make install
